784129b709b326eb77c593d26d8c785c7a7f0669
iagogfnzy <iagogfn@yahoo.com>
2021-05-31 20:33:02 -0300

    att
    att

---
+++ root/script.js
@@ -0,0 +1,17 @@
+//console.log('hi');
+
+
+function autenticar() {
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (usuario_db.dados.email === email && usuario_db.dados.senha === password) {
+        //  location.href = location.href.replace('login', '');
+        alert('Login efetudado com sucesso');
+    } else {
+        alert('Email ou senha inválidos');
+    }
+    return false;
+
+}
